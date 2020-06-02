b33a7d9f269f6eb207b49eb706217ecc475980e0
Daan Toor <d.toorneman@hotmail.com>
2020-06-02 11:35:28 +0200

    logincontroller

--- src/testgroep/Main.java
+++ src/testgroep/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Daan");
+
+        LoginController loginController = new LoginController();
+
     }
 }
